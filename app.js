9a73ee5e5f164ccaa6a1b35ea5b079925d475386
Glory-Lyka Okposin <lykaedem11@gmail.com>
2024-02-24 12:47:48 +0100

    express & logger

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require("express");
 const PORT = 3030;
 const app = express();
 const authorRoute = require("./author.route");
+const logger = require("./logger");
 
 // Middleware
 app.use(express.json());
+app.use(logger);
 app.use("/author", authorRoute);
 
 app.get("/", (req, res) => {
-  res.send(`Hello People`);
+  res.send(`Hello Readers , welcome to this library `);
+});
+
+app.all("*", (req, res) => {
+  res.send(`Page doesn't exit, Kindly navigate to the accurate route`);
 });
 app.listen(PORT, () => {
   console.log(`Server is listening at ${PORT}`);
--- author.route.js
+++ author.route.js
@@ -8,11 +8,19 @@ router.get("/", (req, res) => {
 router.get("/:authorId", (req, res) => {
   console.log(req.param);
   console.log(req.query);
-  res.json({ name: "Author not found!", page: 230 });
+  res.json({ name: "Xela Yadnus", page: 230 });
 });
 
 router.post("/", (req, res) => {
-  res.json({ message: "Author created successfully", books: req.body });
+  res.json({ message: "Author added successfully", books: req.body });
+});
+
+router.put("/", (req, res) => {
+  res.json({ message: "Author updated successfully" });
+});
+
+router.delete("/", (req, res) => {
+  res.json({ message: "Author DELETED" });
 });
 
 module.exports = router;
